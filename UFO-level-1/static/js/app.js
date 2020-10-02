79c9816a6d54d5ed37d6e60f17e4c36f2dbb0a7b
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-02 16:30:40 -0700

    working on filters

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,3 +15,27 @@ tableData.forEach((ufoData) => {
     cell.text(value);
   });
 });
+
+const runEnter = () => {
+
+  // Select the button and filter references
+  let button = d3.select("#filter-btn");
+  
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  // Get the value property of the input element
+  let dateinput = d3.select("#datetime"),
+    dateValue = inputElement.property("value");
+
+  console.log(dateinput);
+  console.log(dateValue);
+  
+  let filteredDate = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  console.log(filteredDate);
+
+// Create event handlers 
+button.on("click", runEnter);
