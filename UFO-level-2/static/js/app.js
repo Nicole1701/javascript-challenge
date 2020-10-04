e5571efa52afe4cf81c35e86a1063dc2cd0aea1e
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-03 17:39:00 -0700

    working on level 2 filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,16 +7,29 @@ console.log(data);
 // Get a reference to the table body
 let tbody = d3.select("tbody");
 
-// Populate the table in html
-tableData.forEach((ufoData) => {
-  let row = tbody.append("tr");
-  Object.values(ufoData).forEach((value) => {
-    let cell = row.append("td");
-    cell.text(value);
+// Create function to show full table
+function showTable() {
+  tableData.forEach((ufoData) => {
+    let row = tbody.append("tr");
+    Object.values(ufoData).forEach((value) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
+}
 
-// Select the button
+// Show table when loading page
+showTable();
+
+// tableData.forEach((ufoData) => {
+//   let row = tbody.append("tr");
+//   Object.values(ufoData).forEach((value) => {
+//     let cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+// Select the buttons
 button = d3.select("#filter-btn");
 resetButton = d3.select("#reset-btn");
 
@@ -28,54 +41,45 @@ const runEnter = () => {
   tbody.html("");
 
   // Define the input element and value property
-  let dateInput = d3.select("#datetime");
-  let cityInput = d3.select("#city");
-  let stateInput = d3.select("#state");
-  let countryInput = d3.select("#country");
-  let shapeInput = d3.select("#shape");
+  // let dateInput = d3.select("#datetime");
+  // let cityInput = d3.select("#city");
+  // let stateInput = d3.select("#state");
+  // let countryInput = d3.select("#country");
+  // let shapeInput = d3.select("#shape");
 
-  let dateValue = dateInput.value.trim().toLowerCase();
-  let cityValue = dateInput.value.trim().toLowerCase();
-  let stateValue = dateInput.value.trim().toLowerCase();
-  let countryValue = dateInput.value.trim().toLowerCase();
-  let shapeValue = dateInput.value.trim().toLowerCase();
-
-  // Check inputValue
-  console.log(inputValue);
+  // let dateValue = dateInput.value;
+  // let cityValue = cityInput.property("value").trim().toLowerCase();
+  // let stateValue = stateInput.property("value").trim().toLowerCase();
+  // let countryValue = countryInput.property("value").trim().toLowerCase();
+  // let shapeValue = shapeInput.property("value").trim().toLowerCase();
 
   // Filter data
-  let filteredData = tableData.filter(
-    (sightings) =>
-      sightings.datetime === dateValue &&
-      sightings.city === cityValue &&
-      sightings.state === stateValue &&
-      sightings.country === countryValue &&
-      sightings.shape === shapeValue
-  );
+  // let filteredData = tableData.filter(
+  //   (sightings) =>
+  //     sightings.datetime === dateValue &&
+  //     sightings.city === cityValue &&
+  //     sightings.state === stateValue &&
+  //     sightings.country === countryValue &&
+  //     sightings.shape === shapeValue
+  // );
+  // stackoverflow.com/questions/31831651/javascript-filter-array-multiple-conditions/44807918
+  let filteredData = {
+    datetime: d3.select("#datetime").property("value"),
+    city: d3.select("#city").property("value").trim().toLowerCase(),
+    state: d3.select("#state").property("value").trim().toLowerCase(),
+    country: d3.select("#country").property("value").trim().toLowerCase(),
+    shape: d3.select("#shape").property("value").trim().toLowerCase(),
+  };
 
-  // Check filteredData
-  console.log(filteredData);
-
-  filteredData.forEach((selection) => {
-    let row = tbody.append("tr");
-    Object.values(selection).forEach((value) => {
-      let cell = row.append("td");
-      cell.text(value);
-    });
-  });
-};
-
-// Reset Table Function
-const resetTable = () => {
-  tableData.forEach((ufoData) => {
-    let row = tbody.append("tr");
-    Object.values(ufoData).forEach((value) => {
-      let cell = row.append("td");
-      cell.text(value);
-    });
-  });
+  //   filters.forEach((selection) => {
+  //     let row = tbody.append("tr");
+  //     Object.values(selection).forEach((value) => {
+  //       let cell = row.append("td");
+  //       cell.text(value);
+  //     });
+  //   });
 };
 
 // Create event handlers
 button.on("click", runEnter);
-resetButton.on("click", resetTable);
+resetButton.on("click", showTable);
