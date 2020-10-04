40fdab4d04343e9d90bc11463d058496ff068757
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-03 19:30:59 -0700

    finally got the multiple filters to workgit add .!

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -21,14 +21,6 @@ function showTable() {
 // Show table when loading page
 showTable();
 
-// tableData.forEach((ufoData) => {
-//   let row = tbody.append("tr");
-//   Object.values(ufoData).forEach((value) => {
-//     let cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
-
 // Select the buttons
 button = d3.select("#filter-btn");
 resetButton = d3.select("#reset-btn");
@@ -40,45 +32,48 @@ const runEnter = () => {
   // Clear the previous table
   tbody.html("");
 
-  // Define the input element and value property
-  // let dateInput = d3.select("#datetime");
-  // let cityInput = d3.select("#city");
-  // let stateInput = d3.select("#state");
-  // let countryInput = d3.select("#country");
-  // let shapeInput = d3.select("#shape");
+  // Define the input elements
+  let dateInput = d3.select("#datetime");
+  let cityInput = d3.select("#city");
+  let stateInput = d3.select("#state");
+  let countryInput = d3.select("#country");
+  let shapeInput = d3.select("#shape");
 
-  // let dateValue = dateInput.value;
-  // let cityValue = cityInput.property("value").trim().toLowerCase();
-  // let stateValue = stateInput.property("value").trim().toLowerCase();
-  // let countryValue = countryInput.property("value").trim().toLowerCase();
-  // let shapeValue = shapeInput.property("value").trim().toLowerCase();
+  // Define the values and trim/lower case
+  let dateValue = dateInput.property("value");
+  let cityValue = cityInput.property("value").trim().toLowerCase();
+  let stateValue = stateInput.property("value").trim().toLowerCase();
+  let countryValue = countryInput.property("value").trim().toLowerCase();
+  let shapeValue = shapeInput.property("value").trim().toLowerCase();
 
-  // Filter data
-  // let filteredData = tableData.filter(
-  //   (sightings) =>
-  //     sightings.datetime === dateValue &&
-  //     sightings.city === cityValue &&
-  //     sightings.state === stateValue &&
-  //     sightings.country === countryValue &&
-  //     sightings.shape === shapeValue
-  // );
+  // Grab the data from the filters
   // stackoverflow.com/questions/31831651/javascript-filter-array-multiple-conditions/44807918
-  let filteredData = {
-    datetime: d3.select("#datetime").property("value"),
-    city: d3.select("#city").property("value").trim().toLowerCase(),
-    state: d3.select("#state").property("value").trim().toLowerCase(),
-    country: d3.select("#country").property("value").trim().toLowerCase(),
-    shape: d3.select("#shape").property("value").trim().toLowerCase(),
-  };
+  let filteredData = tableData.filter(function (filter) {
+    return (
+      (filter.datetime === dateValue || dateValue == "") &&
+      (filter.city === cityValue || cityValue == "") &&
+      (filter.state === stateValue || stateValue == "") &&
+      (filter.country === countryValue || countryValue == "") &&
+      (filter.shape === shapeValue || shapeValue == "")
+    );
+  });
 
-  //   filters.forEach((selection) => {
-  //     let row = tbody.append("tr");
-  //     Object.values(selection).forEach((value) => {
-  //       let cell = row.append("td");
-  //       cell.text(value);
-  //     });
-  //   });
+  // Console.log the filteredData
+  console.log(filteredData);
+
+  // Create table with only filtered data
+  filteredData.forEach((selection) => {
+    let row = tbody.append("tr");
+    Object.values(selection).forEach((value) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
 };
+
+// function (clearFilters){
+
+// }
 
 // Create event handlers
 button.on("click", runEnter);
